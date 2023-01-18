2392bb9f38d6db796162693d0729d2f0d5fe8d7a
Balraj0017 <95099000+Balraj0017@users.noreply.github.com>
2023-01-18 18:55:51 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,12 @@
-Get-Content -Path "./test.json"
+ #Get-Content -Path "./test.json"
+ 
+ #converting the from the json present on git repo
+$response = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Balraj0017/ADO_cred/main/test.json' -UseBasicParsing
+$response = $response | ConvertFrom-Json 
+
+$secretvalue = ConvertTo-SecureString $response.SecretKey -AsPlainText -Force
+$ApplicationName = $response.ApplicationName -Replace '\.',''
+Set-AzKeyVaultSecret -VaultName "demo1cred" -Name $ApplicationName -SecretValue $secretvalue
+
+#reading the secret
+Get-AzKeyVaultSecret -VaultName "demo1cred" -Name $ApplicationName -AsPlainText
